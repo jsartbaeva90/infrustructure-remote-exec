259f962e6d6384654bf16a6a659fde686add8548
jsartbaeva <jsartbaeva91@gmail.com>
2019-08-07 19:50:11 -0500

    updates

--- jenkins.tf
+++ jenkins.tf
@@ -1,15 +1,16 @@
 resource "aws_instance" "jenkins" {
-    ami ="${var.ami}"
-    instance_type ="${var.instance_type}"
-    key_name ="${aws_key_pair.developer_key.id}"
-    associate_public_ip_address =true
-    security_groups =["${aws_security_group.jenkins.name}"]
+    ami = "${var.ami}"
+    instance_type = "${var.instance_type}"
+    key_name =  "${aws_key_pair.developer_key.id}"
+    associate_public_ip_address = true
+    security_groups = ["${aws_security_group.jenkins.name}"]
+
     provisioner "remote-exec" {
         connection {
-            host ="${self.associate_public_ip}"
-            type ="ssh"
-            user ="${var.user}"
-            private_key ="${file("~/.ssh/id_rsa")}"
+            host = "${self.public_ip}"
+            type = "ssh"
+            user  = "${var.user}"
+            private_key = "${file("~/.ssh/id_rsa")}"
         }
         inline  = [
             "sudo yum install java-1.8.0-openjdk-devel -y",
